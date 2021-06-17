f182eac998fb6c3de7b68d625772bcb0984b5d4e
brboba <76227394+brboba@users.noreply.github.com>
2021-06-17 07:19:49 +0300

    lesson 4 1

--- les4.go
+++ les4.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 func main() {
 
+	if len(mystring) > 0 {
+	}
+
+	if mystring != "" {
+	}
+
+	ints := []int{7, 2, 4}
+	sort.Ints(ints)
+	fmt.Println("Ints:   ", ints)
 }

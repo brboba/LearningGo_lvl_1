0e9103cda66ca1caf1d7daede6dd500afa8b81f2
brboba <76227394+brboba@users.noreply.github.com>
2021-06-21 02:10:35 +0300

    Update les5.go
    5 урок с  map  не взлетело подумаю еще

--- les5.go
+++ les5.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func fibonacci(i int) (ret int) {
 	if i == 0 {
 		return 0
@@ -9,12 +11,28 @@ func fibonacci(i int) (ret int) {
 	}
 	return fibonacci(i-1) + fibonacci(i-2)
 }
+func fibonacci1(i int) (ret int) {
+	m := make(map[int]int)
+
+	if m[i] == 0 {
+		return 0
+		println(m[i])
+	}
+	if m[i] == 1 {
+		return 1
+		println(m[i])
+	}
+	return fibonacci1(m[i]-1) + fibonacci1(m[i]-2)
+}
+
 
 func main() {
 	var N int
-	N = 7
-	//	m := make(map[int]int)
 
-	println("Вычислим чему равно ", N, " число в последовательности фибоначи")
+	println("Вычислим чему равно N число в последовательности фибоначи," +
+		" для этого введите N")
+	fmt.Scanln(&N)
+
 	println(N, "число в последовательности фибоначи будет равно", fibonacci(N))
+	println(N, "число в последовательности фибоначи будет равно", fibonacci1(N))
 }

d6695fbef1724f06b2e36e502c17f8f47412692c
brboba <76227394+brboba@users.noreply.github.com>
2021-06-21 01:43:40 +0300

    lesson 5 без  map

--- les5.go
+++ les5.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+func fibonacci(i int) (ret int) {
+	if i == 0 {
+		return 0
+	}
+	if i == 1 {
+		return 1
+	}
+	return fibonacci(i-1) + fibonacci(i-2)
+}
 
+func main() {
+	var N int
+	N = 7
+	//	m := make(map[int]int)
+
+	println("Вычислим чему равно ", N, " число в последовательности фибоначи")
+	println(N, "число в последовательности фибоначи будет равно", fibonacci(N))
 }

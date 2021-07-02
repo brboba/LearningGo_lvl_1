8f1940afefb7df01094260ed5dd53c2896436827
brboba <76227394+brboba@users.noreply.github.com>
2021-07-02 08:10:50 +0300

    Update test.go

--- test.go
+++ test.go
@@ -2,15 +2,36 @@ package main
 
 import "fmt"
 
+func fibGen() func(int) int {
+	var cache = map[int]int{}
+
+	var fibRec func(int) int
+
+	fibCache := func(n int) int {
+		v, ok := cache[n]
+		if !ok {
+			v = fibRec(n)
+			cache[n] = v
+		}
+		return v
+	}
+
+	fibRec = func(n int) int {
+		fmt.Print(n, " ")
+		if n <= 1 {
+			return n
+		}
+
+		return fibCache(n-1) + fibCache(n-2)
+	}
+
+	return fibRec
+}
+
 func main() {
-	var creature string = "shark"
-	var pointer *string = &creature
-
-	fmt.Println("creature =", creature)
-	fmt.Println("pointer =", pointer)
-
-	fmt.Println("*pointer =", *pointer)
-
-	*pointer = "jellyfish"
-	fmt.Println("*pointer =", *pointer)
+	fib := fibGen()
+	/*for i := 0; i < 10; i++ {
+	  fmt.Print(fib(i), ", ")
+	  }*/
+	fib(9)
 }

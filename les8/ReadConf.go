a0f551a7a914fcc9b62aeeaf1b9fd10ffffc5ca1
brboba <76227394+brboba@users.noreply.github.com>
2021-07-06 13:10:58 +0300

    Create ReadConf.go
    update

---
+++ les8/ReadConf.go
@@ -0,0 +1,93 @@
+package ReadConf
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var Hellow = "Hellow config"
+
+type Config struct {
+	Port        int
+	DbUrl       string
+	JaegerUrl   string
+	SentryUrl   string
+	KafkaBroker string
+	SomeAppId   string
+	SomeAppKey  string
+}
+
+func GetConfig(configTypes string) (*Config, error) {
+	switch configTypes {
+	case "env":
+		return getEnvConfig()
+	default:
+		return nil, fmt.Errorf("Заданный вами тип не поддерживается")
+	}
+}
+
+func getEnvConfig() (*Config, error) {
+	config := &Config{}
+
+	port, err := getEnvVar("PORT")
+	if err != nil {
+		return nil, err
+	}
+	portInt, err := strconv.ParseInt(port, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	config.Port = int(portInt)
+
+	dbUrl, err := getEnvVar("DB_URL")
+	if err != nil {
+		return nil, err
+	}
+	config.DbUrl = dbUrl
+
+	jaegerUrl, err := getEnvVar("JAEGER_URL")
+	if err != nil {
+		return nil, err
+	}
+	config.JaegerUrl = jaegerUrl
+
+	sentryUrl, err := getEnvVar("SENTRY_URL")
+	if err != nil {
+		return nil, err
+	}
+	config.SentryUrl = sentryUrl
+
+	kafkaBroker, err := getEnvVar("KAFKA_BROKER")
+	if err != nil {
+		return nil, err
+	}
+	config.KafkaBroker = kafkaBroker
+
+	someAppId, err := getEnvVar("SOME_APP_ID")
+	if err != nil {
+		return nil, err
+	}
+	config.SomeAppId = someAppId
+
+	someAppKey, err := getEnvVar("SOME_APP_KEY")
+	if err != nil {
+		return nil, err
+	}
+	config.SomeAppKey = someAppKey
+
+	return config, nil
+}
+
+func getEnvVar(envVar string) (string, error) {
+	value := os.Getenv(envVar)
+	if value == "" {
+		return "", fmt.Errorf("you should define env-var \"%s\"", envVar)
+	}
+	return value, nil
+}
+
+
+
+
+

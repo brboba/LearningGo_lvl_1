a130b4871e1c55eef830ca142399c5c6de8eec6c
brboba <76227394+brboba@users.noreply.github.com>
2021-07-18 13:07:32 +0300

    Update les4.go
    12

--- les4.go
+++ les4.go
@@ -8,6 +8,7 @@ import (
 )
 
 func main() {
+<<<<<<< Updated upstream
 	var str string
 	fmt.Println("Введите последовательность целых чисел в формате: \"7,2,47,242,475,12,42\", если пропустить" +
 		"ввод то будет использована последовательность из примера ")
@@ -21,6 +22,27 @@ func main() {
 	//ttt = {7,2,47,242,475,12,42}
 	//}
 
+=======
+
+	var str string
+
+	fmt.Println("введите последовательность для сортировки в виде: \"1,33,5,56,8,42,11\" " +
+		"или нажмите enter чтобы использовать последовательность из примера")
+	fmt.Scanln(&str)
+
+	 fmt.Println("считали:      ", str)
+
+	//if len(str) == 0 {			 // метод проверки 1
+	//	str= "1,33,5,56,8,42,11"
+	//}
+
+	if str == "" {					 // метод проверки 2
+		str = "1,33,5,56,8,42,11"
+	}
+
+	fmt.Println("итого:      ", str)
+
+>>>>>>> Stashed changes
 	strs := strings.Split(str, ",")
 	var ints []int
 	for _, s := range strs {
@@ -31,7 +53,14 @@ func main() {
 	}
 
 	fmt.Println("массив до сортировки:      ", ints)
+<<<<<<< Updated upstream
 
 	sort.Ints(ints)
 	fmt.Println("массив после сортировки:   ", ints)
 }
+=======
+	sort.Ints(ints)
+	fmt.Println("массив после сортировки:   ", ints)
+
+}
+>>>>>>> Stashed changes

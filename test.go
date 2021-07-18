ca4e1de35ceab88bd27abe65bdf2f5e46fa98e2f
brboba <76227394+brboba@users.noreply.github.com>
2021-07-18 13:07:36 +0300

    Update test.go

--- test.go
+++ test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+<<<<<<< Updated upstream
 	"io"
 	"log"
 	"os"
@@ -30,5 +31,33 @@ func main() {
 			break           // выходим из цикла
 		}
 		fmt.Print(string(ConfRead[:n]))
+=======
+	"math/big"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().Unix()) // случайные числа действительно случайные
+	var N = rand.Intn(10000)       //присваиваем случайные числа
+	fmt.Println("возьмем случайное число N = ", N)
+
+	if N < 2 {
+		fmt.Println("число N меньше 2")
+	}
+
+	if N == 2 {
+		fmt.Println("2")
+	}
+
+	if N > 2 {
+		for i := 2; (i * i) < (N + 1); i++ {
+			j := big.NewInt(int64(i))
+			isPrime := j.ProbablyPrime(1)
+			if isPrime == true{
+				fmt.Println(i)
+			}
+		}
+>>>>>>> Stashed changes
 	}
 }

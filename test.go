41cc8be277412c0615e0f543136c401bf4745b83
brboba <76227394+brboba@users.noreply.github.com>
2021-07-18 11:11:13 +0300

    Update test.go

--- test.go
+++ test.go
@@ -1,39 +1,34 @@
 package main
 
-import "fmt"
-
-func fibGen() func(int) int {
-	var cache = map[int]int{}
-
-	var fibRec func(int) int
-
-	fibCache := func(n int) int {
-		v, ok := cache[n]
-		if !ok {
-			v = fibRec(n)
-			cache[n] = v
-		}
-		return v
-	}
-
-	fibRec = func(n int) int {
-		fmt.Print(n, " ")
-		if n <= 1 {
-			return n
-		}
-
-		return fibCache(n-1) + fibCache(n-2)
-	}
-
-	return fibRec
-}
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
-	fib := fibGen()
-	/*for i := 0; i < 10; i++ {
-	  fmt.Print(fib(i), ", ")
-	  }*/
-	fib(9)
+
+	// Пробуем открыть файл
+	file, err := os.Open("les9/conf.yaml")
+	if err != nil {
+		log.Fatalf("Не могу открыть файл: %v", err)
+	}
+
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			log.Printf("Не могу закрыть файл: %v", err)
+		}
+	}()
+
+	ConfRead:= make([]byte, 64)
+
+	for{
+		n, err := file.Read(ConfRead)
+		if err == io.EOF{   // если конец файла
+			break           // выходим из цикла
+		}
+		fmt.Print(string(ConfRead[:n]))
+	}
 }
-
-

e267e84dade0e6d8a61ffebd4afe06ded5056be4
brboba <76227394+brboba@users.noreply.github.com>
2021-07-18 11:11:24 +0300

    Update greet.go

--- test2/greet.go
+++ test2/greet.go
@@ -1,24 +1,11 @@
 package greet
 
-import "fmt"
+var Config string
 
-var Shark = "Sammy"
-
-type Octopus struct {
-	Name  string
-	Color string
+func GetHellow() (Config, error) {
+	return getArgumentsConfig(), nil
 }
 
-func (o Octopus) String() string {
-	return fmt.Sprintf("The octopus's name is %q and is the color %s.", o.Name, o.Color)
-}
-
-func (o *Octopus) Reset() {
-	o.Name = "newName"
-	fmt.Println("Change Name", o.Name)
-	o.Color = ""
-}
-
-func Hello() {
-	fmt.Println("Hello, World!")
+func getArgumentsConfig {
+	return 1
 }
